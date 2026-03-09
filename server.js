62ee533826ad15eee7db64e93881f348baac7777
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2026-03-09 18:27:34 +0530

    Connect mongoDB

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDb from "./config/connectDb.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT
 
 app.get("/", (req,res) => res.send("server Running Sucessfully..."))
+
+connectDb();
 
 app.listen(port, ()=>{
     console.log(`Server Running on port ${port}`)
