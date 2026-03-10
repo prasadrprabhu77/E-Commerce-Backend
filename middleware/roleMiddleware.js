4ac18eaedf1127cd4a00822a876689226a90a203
prasadrprabhu77 <prasadrprabhu77@gmail.com>
2026-03-10 18:17:40 +0530

    rolemiddleware created

---
+++ middleware/roleMiddleware.js
@@ -0,0 +1,26 @@
+const roleMiddleware = (allowedRole) =>{
+    return (req,res,next) =>{
+        try {
+        if(req.user){
+            return res.status(401).send({
+                message:"Not authorized, User not found"
+            })
+        }
+
+        if(allowedRole !== req.user.role){
+            return res.status(403).send({
+                message: "Access denied. You do not have permission"
+            })
+        }
+
+        next()
+    } catch (error) {
+        res.status(500).json({
+        message: "Server error",
+        error: error.message,
+      });
+    }
+  }
+}
+
+export default roleMiddleware;
